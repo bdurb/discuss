dc8f2f9ad07ae20009bc2ed490b6abb729183351
bdurb <brian@rpmgreenville.com>
2018-09-03 15:38:07 -0700

    set up auth around comments via socket connection

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -10,8 +10,12 @@ const createSocket = (topicId) => {
   channel.join()
     .receive("ok", resp => {
        renderComments(resp.comments);
-      })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+    })
+    .receive("error", resp => {
+       console.log("Unable to join", resp)
+    })
+
+    channel.on(`comments:${topicId}:new`, renderComment)
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value
@@ -22,13 +26,23 @@ const createSocket = (topicId) => {
 
 function renderComments(comments) {
   const renderedComments = comments.map(comment => {
-    return `
+    return commentTemplate(comment)
+  })
+  document.querySelector('.collection').innerHTML = renderedComments.join('')
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+  
+  document.querySelector('.collection').innerHTML += renderedComment
+}
+
+function commentTemplate(comment) {
+  return `
       <li class="collection-item">
         ${comment.content}
       </li>
     `
-  })
-  document.querySelector('.collection').innerHTML = renderedComments.join('')
 }
 
 
